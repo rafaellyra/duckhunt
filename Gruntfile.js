4d436ea721482bafe1bc4c0e4df2654505bc08e2
Rafael Lyra <rafael@rafaellyra.com>
2013-09-16 02:29:45 -0300

    Add dist and dev compass tasks and grunt uglify task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,33 @@ module.exports = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         compass: {
-            dist: {
+            dev: {
                 options: {
                     sassDir: 'assets/styles/sass',
                     cssDir: 'assets/styles/css',
-                    imagesDir: 'assets/images/compass',
+                    imagesDir: 'assets/images/',
+                    fontsDir: 'assets/fonts/',
+                    outputStyle: 'expanded',
+                    generatedImagesDir: 'assets/images/sprites/',
+                    relativeAssets: true
+                }
+            },
+            dist: {
+                options: {
+                    sassDir: 'assets/styles/sass',
+                    cssDir: 'dist/css/',
+                    imagesDir: 'assets/images/',
                     fontsDir: 'assets/fonts/',
                     outputStyle: 'compressed',
+                    generatedImagesDir: 'dist/img/',
                     relativeAssets: true
+                }
+            }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    'dist/main.min.js': ['assets/scripts/**.*js']
                 }
             }
         },
@@ -30,6 +49,7 @@ module.exports = function (grunt) {
                 options: {
                     port: 9001,
                     keepalive: true,
+                    hostname: '*',
                     open: true
                 }
             }
@@ -50,10 +70,12 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('server', ['connect:server']);
     grunt.registerTask('dev', ['csswatch']);
+    grunt.registerTask('build', ['compass:dist', 'uglify:dist']);
 
     grunt.registerTask('csswatch', ['watch:styles']);
-    grunt.registerTask('buildCSS', ['compass:dist', 'csslint']);
+    grunt.registerTask('buildCSS', ['compass:dev', 'csslint']);
 };
