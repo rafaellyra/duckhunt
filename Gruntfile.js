d66dd92d06d35d2ae895d1ba066cca062c8eb179
Rafael Lyra <rafael@rafaellyra.com>
2013-09-29 19:25:24 -0300

    Update Gruntfile.js Add matchdep to auto load npm plugins. Remove dist folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,31 @@
 module.exports = function (grunt) {
     'use strict';
+
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         compass: {
             dev: {
                 options: {
-                    sassDir: 'assets/styles/sass',
-                    cssDir: 'assets/styles/css',
+                    sassDir: 'assets/styles/sass/',
+                    cssDir: 'assets/styles/css/',
                     imagesDir: 'assets/images/',
                     fontsDir: 'assets/fonts/',
                     outputStyle: 'expanded',
                     generatedImagesDir: 'assets/images/sprites/',
                     relativeAssets: true
                 }
-            },
-            dist: {
-                options: {
-                    sassDir: 'assets/styles/sass',
-                    cssDir: 'dist/css/',
-                    imagesDir: 'assets/images/',
-                    fontsDir: 'assets/fonts/',
-                    outputStyle: 'compressed',
-                    generatedImagesDir: 'dist/img/',
-                    relativeAssets: true
-                }
             }
         },
+        clean: {
+            'no-write': true,
+            compass: ['assets/styles/css/', 'assets/images/sprites/']
+        },
         uglify: {
-            dist: {
+            dev: {
                 files: {
-                    'dist/main.min.js': ['assets/scripts/**.*js']
+                    'assets/scripts/main.min.js': ['assets/scripts/main.optimized.js']
                 }
             }
         },
@@ -39,7 +35,7 @@ module.exports = function (grunt) {
             }
         },
         jshint: {
-            all: ['Gruntfile.js', 'assets/scripts/**/*.js'],
+            all: ['Gruntfile.js', 'assets/scripts/require.config.js', 'assets/scripts/main.js', 'assets/scripts/modules/*.js'],
             options: {
                 jshintrc: '.jshintrc'
             }
@@ -48,19 +44,20 @@ module.exports = function (grunt) {
             server: {
                 options: {
                     port: 9001,
-                    keepalive: true,
-                    hostname: '*',
-                    open: true
+                    hostname: 'localhost',
+                    open: true,
+                    livereload: true
                 }
             }
         },
         watch: {
             styles: {
                 files: ['assets/styles/sass/**/*'],
-                tasks: ['buildCSS', 'csslint'],
-                options: {
-                    livereload: true
-                }
+                tasks: ['buildCSS']
+            },
+            javascript: {
+                files: ['assets/styles/scripts/main.js', 'assets/styles/scripts/modules/*.js'],
+                tasks: ['buildJS']
             }
         },
         requirejs: {
@@ -75,19 +72,14 @@ module.exports = function (grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.loadNpmTasks('grunt-contrib-csslint');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-connect');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-requirejs');
-
-
     grunt.registerTask('server', ['connect:server']);
-    grunt.registerTask('dev', ['csswatch']);
+
+    grunt.registerTask('buildCSS', ['clean:compass', 'compass:dev', 'csslint']);
+    grunt.registerTask('buildJS', ['jshint', 'requirejs', 'uglify']);
+
+    grunt.registerTask('dev', ['connect', 'watch:styles', 'watch:javascript']);
     grunt.registerTask('build', ['compass:dist', 'uglify:dist']);
 
-    grunt.registerTask('csswatch', ['watch:styles']);
-    grunt.registerTask('buildCSS', ['compass:dev', 'csslint']);
+
+
 };
